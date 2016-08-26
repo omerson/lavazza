c5c2ce854b91aea6829a2cd8ed51115c52d52407
Edis Omerovic <edis.omerovic@softhouse.se>
2016-08-26 21:51:12 +0200

    Add read locations

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -551,7 +551,7 @@ function drawItemSpecific(category, json, a){
 function quickView(id){
     $.ajax({
         type: 'POST',
-        url: 'assets/external/_modal.html',
+        url: 'assets/external/_modal'+id+'.html',
         data: id,
         success: function (data) {
             // Create HTML element with loaded data
